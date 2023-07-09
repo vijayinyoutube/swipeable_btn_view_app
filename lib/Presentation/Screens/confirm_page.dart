a14f0e76474c825cee1fffd2f710d4212882921f
vijayinyoutube <vijaythevirtue@gmail.com>
2023-07-09 19:16:14 +0530

    refactor

---
+++ lib/Presentation/Screens/confirm_page.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class ConfirmationPage extends StatefulWidget {
+  const ConfirmationPage({super.key});
+
+  @override
+  State<ConfirmationPage> createState() => _ConfirmationPageState();
+}
+
+class _ConfirmationPageState extends State<ConfirmationPage>
+    with TickerProviderStateMixin {
+  late final AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0x00962e),
+      body: Center(
+        child: Lottie.asset(
+          "assets/Images/check.json",
+          controller: _controller,
+          onLoaded: (composition) {
+            _controller
+              ..duration = composition.duration
+              ..forward();
+          },
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/Presentation/Screens/home_page.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
+import 'package:swipeable_btn_view_app/Presentation/Screens/confirm_page.dart';
+import 'package:swipeable_button_view/swipeable_button_view.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool isFinished = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(50),
+        child: Center(
+            child: SwipeableButtonView(
+          buttonText: 'Slide to Pay',
+          buttontextstyle: const TextStyle(fontSize: 25, color: Colors.white),
+          buttonWidget: const Icon(
+            Icons.arrow_forward_ios_rounded,
+            color: Colors.grey,
+          ),
+          activeColor: const Color(0xFF009C41),
+          isFinished: isFinished,
+          onWaitingProcess: () {
+            Future.delayed(const Duration(seconds: 2),
+                () => setState(() => isFinished = true));
+          },
+          onFinish: () async {
+            await Navigator.push(
+                context,
+                PageTransition(
+                    type: PageTransitionType.fade,
+                    child: const ConfirmationPage()));
+
+            setState(() {
+              isFinished = false;
+            });
+          },
+        )),
+      ),
+    );
+  }
+}
